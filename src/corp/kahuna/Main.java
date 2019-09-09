c5e64f48b6a896455cca164bb7555380d75bc3f9
Dave Bohrk <dave@ionicsecurity.com>
2019-09-09 12:30:24 -0400

    Updated comments to indicate start of instruction

--- src/corp/kahuna/Main.java
+++ src/corp/kahuna/Main.java
@@ -1,8 +1,35 @@
 package corp.kahuna;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here - beginning of instruction
+
+        Scanner scanner = new Scanner(System.in);
+        Random randomGenerator = new Random();
+
+        int firstNumber = randomGenerator.nextInt(8) +2;
+        int secondNumber = randomGenerator.nextInt(8) +2;
+
+        var subtraction = randomGenerator.nextInt(8) +2;
+        int answer = firstNumber * secondNumber - subtraction;
+        String prompt = ". Press ENTER when you're ready to continue";
+
+        System.out.println("Think of a number between 1 and 10" + prompt);
+        scanner.nextLine();
+        System.out.println("Multiply your number by " + firstNumber + prompt);
+        scanner.nextLine();
+        System.out.println("Now multiply the result by " + secondNumber + prompt);
+        scanner.nextLine();
+        System.out.print("Divide the result by the number you originally thought of " + prompt);
+        scanner.nextLine();
+        System.out.println("Now subtract " + subtraction + prompt);
+        scanner.nextLine();
+
+        System.out.println("The answer is " + answer);
+
+        scanner.close();
     }
 }
